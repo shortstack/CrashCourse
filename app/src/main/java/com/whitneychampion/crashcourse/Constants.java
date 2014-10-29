dccaab61db3a513e12073b83c6a9900db67adc80
Whitney Champion <whitney.ellis.champion@gmail.com>
2014-10-29 15:39:59 -0400

    commenting constants

--- app/src/main/java/com/whitneychampion/crashcourse/Constants.java
+++ app/src/main/java/com/whitneychampion/crashcourse/Constants.java
@@ -6,9 +6,14 @@ package com.whitneychampion.crashcourse;
 
 public class Constants {
 
+    // error messages
     public static final String TOKEN_AUTH_ERROR_MESSAGE = "Token validation error.";
-    public static final String API_URL = "http://shortstack.github.io/crashcourse/";
     public static final String API_ERROR_MESSAGE = "\"errorMessage\":";
+
+    // api
+    public static final String API_URL = "http://shortstack.github.io/crashcourse/";
+
+    // endpoints
     public static final String GET_RESTAURANTS = "restaurants.json";
 
 }
